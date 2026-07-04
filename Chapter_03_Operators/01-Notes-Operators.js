75680367299438bdb5e6296930775728be48c08b
Akshay Patel <akshay.patel@thesunflowerlab.com>
2026-07-05 00:44:38 +0530

    Update notes on variables and operators

---
+++ Chapter_03_Operators/01-Notes-Operators.js
@@ -0,0 +1,19 @@
+/*
+
+What is Oeratores?
+Operators in JavaScript are special symbols or keywords that perform operations on one or more values (called operands) and produce a result. They are used to calculate values, compare data, assign values, test conditions, and manipulate variables.
+
+
+| Operator Type       | Definition                                        | Example                         | Returns                |
+| ------------------- | ------------------------------------------------- | ------------------------------- | ---------------------- |
+| Arithmetic          | Performs mathematical calculations.               | `10 + 5`                        | `15`                   |
+| Assignment          | Assigns or updates values in variables.           | `x += 5`                        | Updated variable value |
+| Comparison          | Compares two values and returns a Boolean result. | `10 > 5`                        | `true`                 |
+| Logical             | Combines or negates conditions.                   | `a > 5 && b < 10`               | `true` or `false`      |
+| Increment/Decrement | Increases or decreases a value by 1.              | `count++`                       | Updated value          |
+| String              | Joins two or more strings.                        | `"Hello" + " World"`            | `"Hello World"`        |
+| Ternary             | A one-line alternative to `if...else`.            | `age >= 18 ? "Adult" : "Minor"` | `"Adult"` or `"Minor"` |
+| Type                | Identifies the data type of a value.              | `typeof 25`                     | `"number"`             |
+
+
+*/
